5f5587fd465fda8ae3a7b1adba96e7dcef24ca74
Киленников Роман <kilennikovroman@gmail.com>
2023-06-02 16:45:35 +0300

    add components ProductList & ProductItem

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,7 @@
 <template>
   <section class="catalog">
-    <ul class="catalog__list">
-      <li class="catalog__item" v-for="(product, index) in products" :key="index">
-        <a class="catalog__pic" href="#">
-          <img :src="product.img" alt="product.title">
-        </a>
 
-        <h3 class="catalog__title">
-          <a href="#">
-            {{ product.title }}
-          </a>
-        </h3>
-
-        <span class="catalog__price">
-          {{ product.price }} ₽
-        </span>
-
-        <ul class="colors colors--black">
-          <li class="colors__item">
-            <label class="colors__label">
-              <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">
-              <span class="colors__value" style="background-color: #73B6EA;">
-              </span>
-            </label>
-          </li>
-          <li class="colors__item">
-            <label class="colors__label">
-              <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
-              <span class="colors__value" style="background-color: #8BE000;">
-              </span>
-            </label>
-          </li>
-          <li class="colors__item">
-            <label class="colors__label">
-              <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
-              <span class="colors__value" style="background-color: #222;">
-              </span>
-            </label>
-          </li>
-        </ul>
-      </li>
-    </ul>
+    <ProductList v-bind:products="products"></ProductList>
 
     <ul class="catalog__pagination pagination">
       <li class="pagination__item">
@@ -92,9 +53,14 @@
 </template>
 
 <script>
-import products from './data/products'
+import products from './data/products';
+import ProductList from './components/ProductList';
+
+
 export default {
   name: 'App',
+  components: { ProductList: ProductList },
+
   data() {
     return {
       products: products,
---
+++ src/components/ProductList.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul class="catalog__list">
+        <li class="catalog__item" v-for="(product, index) in products" :key="index">
+            <ProductItem v-bind:product="product"></ProductItem>
+        </li>
+    </ul>
+</template>
+
+<script>
+import ProductItem from './ProductItem';
+
+export default {
+    props: ['products'],
+    components: { ProductItem: ProductItem },
+}
+</script>
